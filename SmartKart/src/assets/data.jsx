b4fe7deb3f648d289933c9de786dbda6a1a4536a
abdul Rafay <abdulrafay700000@gmail.com>
2026-07-04 04:04:29 +0500

    Mock Data

---
+++ SmartKart/src/assets/data.jsx
@@ -0,0 +1,21 @@
+// src/data.js
+export const mockProducts = [
+  { 
+    id: 1,
+    title: "Black Cotton T-Shirt",
+    category: "Shirts",
+    price: 15, 
+    colors: ["Black"], 
+    sizes: ["S", "M", "L"], 
+    image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" },
+    
+  { id: 2, title: "Blue Denim Jeans", category: "Pants", price: 40, colors: ["Blue", "Dark Blue"], sizes: ["30", "32", "34"], image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg" },
+  { id: 3, title: "White Sneakers", category: "Shoes", price: 60, colors: ["White"], sizes: ["8", "9", "10", "11"], image: "https://fakestoreapi.com/img/71li-ujtlTa._AC_UX679_.jpg" },
+  { id: 4, title: "Leather Jacket", category: "Jackets", price: 120, colors: ["Black", "Brown"], sizes: ["M", "L", "XL"], image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg" },
+  { id: 5, title: "Red Polo Shirt", category: "Shirts", price: 20, colors: ["Red", "White"], sizes: ["M", "L"], image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" },
+  { id: 6, title: "Formal Black Shoes", category: "Shoes", price: 85, colors: ["Black"], sizes: ["7", "8", "9"], image: "https://fakestoreapi.com/img/71li-ujtlTa._AC_UX679_.jpg" },
+  { id: 7, title: "Summer Shorts", category: "Pants", price: 18, colors: ["Khaki", "Green"], sizes: ["30", "32"], image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg" },
+  { id: 8, title: "Graphic Hoodie", category: "Jackets", price: 55, colors: ["Gray", "Black"], sizes: ["S", "M", "L"], image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg" },
+  { id: 9, title: "Sports T-Shirt", category: "Shirts", price: 25, colors: ["Blue", "Neon"], sizes: ["M", "L", "XL"], image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" },
+  { id: 10, title: "Cargo Pants", category: "Pants", price: 35, colors: ["Olive", "Black"], sizes: ["32", "34", "36"], image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg" }
+];
