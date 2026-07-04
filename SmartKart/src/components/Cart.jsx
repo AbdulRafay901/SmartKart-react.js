07663ce5769de2189daa4474fbcf5cc8445b6208
abdul Rafay <abdulrafay700000@gmail.com>
2026-07-04 22:29:21 +0500

    f: rating Logic

--- SmartKart/src/components/Cart.jsx
+++ SmartKart/src/components/Cart.jsx
@@ -1,29 +1,35 @@
 import React from 'react'
-
 import Star from './Star'
 import SimpleStar from './SimpleStar'
 import Button from './Button'
 
-const Cart = ({name,img,price}) => {
+const Cart = ({ name, img, price, id, rating }) => {
+ 
+  const starCount = Math.round(rating?.rate || rating || 0);
+
   return (
-    <div className='col-lg-4'>
+    <div className='col-lg-4' key={id}>
          <div className="cart">
                 <div className="img">
-                     <img src={img}></img>
+                     
+                     <img src={img} alt={name} className="img-fluid" />
                 </div>
                 <div className="content">
                       <p className='name'>{name}</p>
                       <div className="rating">
-                         <SimpleStar />
-                         <SimpleStar />
-                         <SimpleStar />
-                         <SimpleStar />
-                         <Star/>  
+                           
+                          
+                           {Array.from({ length: 5 }).map((_, index) => (
+                               index < starCount 
+                                 ? <SimpleStar key={index} /> 
+                                 : <Star key={index} />
+                           ))}
+
                       </div>
                       <p className='price'>${price}</p>
                       <Button text='Add to Cart'/>
                 </div>
-          </div>
+         </div>
     </div>
   )
 }
--- SmartKart/src/components/Center.jsx
+++ SmartKart/src/components/Center.jsx
@@ -7,10 +7,12 @@ import axios from 'axios';
 
 const Center = () => {
 
-  const [Alldata, setAlldata] = useState()
+  const [Alldata, setAlldata] = useState([])
 
 
- const fetch = async () => {
+
+  
+const fetch = async () => {
      const {data} = await axios.get('https://fakestoreapi.com/products');
      
     const modified = data.map(products => ({
@@ -21,7 +23,6 @@ const Center = () => {
 
     setAlldata(modified)
 
-    console.log(Alldata)
   }
 fetch()
 
@@ -34,7 +35,9 @@ fetch()
         <div className="container-fluid p-0">
               <div className="row gy-3 gx-3">
                     
-                  
+                  {Alldata.slice(0,6).map((elem,idx) => (
+                     <Cart  key={idx} name={elem.title} price={elem.price} img={elem.image} rating={elem.rating.rate}/> 
+                  ))}
                     
               </div>
         </div>
